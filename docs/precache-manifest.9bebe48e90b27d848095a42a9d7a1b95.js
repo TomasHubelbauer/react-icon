cb7114f885b091c0e3aa868b4ee570d73415e28c
Tomas Hubelbauer <tom.hubelbauer@microsoft.com>
2019-03-19 10:50:19 +0100

    Build the project to docs for GitHub Pages demo

---
+++ docs/precache-manifest.9bebe48e90b27d848095a42a9d7a1b95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e8050d02e620ed338de1",
+    "url": "/static/js/main.f59c2075.chunk.js"
+  },
+  {
+    "revision": "88530d0938d993809d3e",
+    "url": "/static/js/2.bd56b2ca.chunk.js"
+  },
+  {
+    "revision": "e8050d02e620ed338de1",
+    "url": "/static/css/main.c7ebcee0.chunk.css"
+  },
+  {
+    "revision": "eeb82f9ab5fe31aad8993fdd56289ce4",
+    "url": "/index.html"
+  }
+];
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <p>
+          This is a demonstration of a project idea described in
+          <a href="https://github.com/TomasHubelbauer/react-icon-component-idea">https://github.com/TomasHubelbauer/react-icon-component-idea</a>
+        </p>
         <Icon plus />
         <Icon minus />
         <Icon plus inCircle />
